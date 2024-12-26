c1fdb4d7d92bf0c6fb5036bc883bd2f0777b768b
Khann777 <nurdinov.d777@gmail.com>
2024-12-26 19:04:52 +0600

    commit

--- account/urls.py
+++ account/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('logout/', UserLogoutView.as_view(), name='logout'),
 
     path('', UserListView.as_view(), name='accounts'),
-    path('/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
-    path('/<int:pk>/update/', UserUpdateView.as_view(), name='user-update'),
-    path('/<int:pk>/delete/', UserDeleteView.as_view(), name='user-delete'),
+    path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('<int:pk>/update/', UserUpdateView.as_view(), name='user-update'),
+    path('<int:pk>/delete/', UserDeleteView.as_view(), name='user-delete'),
 
 ]
