25c80addb9235a7b514a1af7119be6f27ff6849a
Khann777 <nurdinov.d777@gmail.com>
2024-12-28 19:19:23 +0600

    created reviews app and configured music app

--- reviews/models.py
+++ reviews/models.py
@@ -1,2 +1,12 @@
 from django.db import models
+from music.models import Song
+from django.contrib.auth.models import User
 
+class Review(models.Model):
+    music = models.ForeignKey(Song, on_delete=models.CASCADE, related_name='reviews')
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.IntegerField(default=0)
+    text = models.TextField(default='')
+
+    def __str__(self):
+        return f'Review for {self.music.title} by {self.reviewer.username}'
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
-# Create your views here.
+from account.permissions import IsUserOrAdmin
+from .models import Review
+from .serializers import ReviewSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    METHODS = ['PATCH', 'PUT', 'DELETE', 'POST']
+
+    @action(detail=True, methods=['GET'])
+    def get_song_rating(self, request):
+        song_rating = Review.objects.filter(song=request.data['song'])
+        serializer = ReviewSerializer(song_rating, many=True)
+        return Response(serializer.data)
+    def get_permissions(self):
+        if self.request.method in self.METHODS:
+            return [IsUserOrAdmin()]
+        return [permissions.AllowAny()]
